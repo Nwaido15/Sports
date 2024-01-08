b45faf533791e84699d00d6028fea750e8418e9e
Nwaido15 <94997094+Nwaido15@users.noreply.github.com>
2024-01-08 07:17:07 -0600

    Update nfl-scrape-teams.py

--- nfl-scrape-teams.py
+++ nfl-scrape-teams.py
@@ -108,8 +108,8 @@ teamsdf = pd.merge(teamsdf, tmabrevdf, on="FullTm")
 
 # Manually store the Super Bowl winners - I just manually do these given it's a small, annually updated dataset
 data = {'Yr':[2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
-    'Tm':['PIT','IND','NYG','PIT','NOR','GNB','NYG','BAL','SEA','NWE','DEN','NWE','PHI','NWE','KAN','TAM'],
-    'SB':[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]}
+    'Tm':['PIT','IND','NYG','PIT','NOR','GNB','NYG','BAL','SEA','NWE','DEN','NWE','PHI','NWE','KAN','TAM','LAR','KAN'],
+    'SB':[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]}
 sbdf = pd.DataFrame(data)
 teamsdf = pd.merge(teamsdf, sbdf, how="left", left_on=["Yr","Tm"], right_on=["Yr","Tm"])
 teamsdf = teamsdf.replace(np.nan, '', regex=True)
